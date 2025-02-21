b2b38f719941d6c4d9cf83cdcddbc0b0519e3876
KaanErgun <kaanergun@live.com>
2025-02-21 12:37:50 +0300

    form and modal styles are separeted

--- bariwell.php
+++ bariwell.php
@@ -14,12 +14,13 @@ if (!defined('ABSPATH')) {
 define('BARIWELL_PATH', plugin_dir_path(__FILE__));
 define('BARIWELL_URL', plugin_dir_url(__FILE__));
 
-// CSS ve JS dosyalarını yükle
 function bariwell_enqueue_assets() {
-    wp_enqueue_style('bariwell-style', BARIWELL_URL . 'assets/style.css', array(), '1.4.2', 'all');
+    wp_enqueue_style('bariwell-modal-style', BARIWELL_URL . 'assets/modal.css', array(), '1.4.2', 'all');
+    wp_enqueue_style('bariwell-form-style', BARIWELL_URL . 'assets/form.css', array(), '1.4.2', 'all');
     wp_enqueue_script('bariwell-script', BARIWELL_URL . 'assets/script.js', array('jquery'), '1.4.2', true);
 }
 add_action('wp_enqueue_scripts', 'bariwell_enqueue_assets');
+
 
 // Diğer dosyaları dahil et
 require_once BARIWELL_PATH . 'includes/settings.php';
